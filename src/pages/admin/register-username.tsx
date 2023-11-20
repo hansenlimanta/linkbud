70d39db8659df2bfd25317536f4dad67634db43e
Hansen Limanta <hansenlimanta@gmail.com>
2023-11-20 23:51:57 +0700

    improve error styling on register page

--- src/pages/admin/register-username.tsx
+++ src/pages/admin/register-username.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState, useEffect, FormEvent } from "react";
+import { useState, useEffect, FormEvent, use } from "react";
 import { GetServerSidePropsContext } from "next";
 import { getServerAuthSession } from "~/server/auth";
 import { signOut } from "next-auth/react";
@@ -28,10 +28,11 @@ export default function RegisterUsername() {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
-    //   e.preventDefault();
     //   updateUsername.mutate({ username: username });
   };
-
+  useEffect(() => {
+    console.log("errors", errors);
+  }, [errors]);
   return (
     <>
       <Head>
@@ -46,24 +47,48 @@ export default function RegisterUsername() {
         >
           <h1 className="text-5xl font-extrabold">Welcome to Linkbud!</h1>
           <p>Choose your Linkbud username. You can always change it later.</p>
-          <div
-            onClick={() => setIsFocus(true)}
-            className={
-              "my-6 flex h-fit w-full items-center justify-start overflow-hidden rounded-md bg-zinc-300 " +
-              `${isFocus ? "outline outline-offset-1" : "outline-none"}`
-            }
-          >
-            <p className="cursor-default px-4 py-2 pr-0 text-gray-500">
-              link.bud/
-            </p>
-            <span className="w-full">
-              <input
-                type="text"
-                placeholder="Username"
-                {...register("username", { required: true })}
-                className="h-full w-full bg-inherit p-2 focus:outline-none"
-              />
-            </span>
+          <div className="flex w-full flex-col items-start justify-center">
+            <div
+              className={`mt-6 flex h-fit w-full items-center justify-start overflow-hidden rounded-md bg-zinc-300 ${
+                errors.username
+                  ? isFocus
+                    ? "border-2 border-red-600 outline outline-offset-1"
+                    : "border-2 border-red-600 outline-none"
+                  : isFocus
+                  ? "outline outline-offset-1"
+                  : "outline-none"
+              }`}
+            >
+              <p className="cursor-default px-4 py-2 pr-0 text-gray-500">
+                link.bud/
+              </p>
+              <span className="w-full">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  {...register("username", {
+                    required: "Please enter a username",
+                    minLength: {
+                      value: 3,
+                      message: "Username must be at least 3 characters long",
+                    },
+                    onBlur: () => setIsFocus(false),
+                    pattern: {
+                      value: /^[a-zA-Z0-9_.]*$/,
+                      message:
+                        "Usernames may only contain letters, numbers, underscores ('_') and periods ('.')",
+                    },
+                  })}
+                  onFocus={() => setIsFocus(true)}
+                  className="h-full w-full bg-inherit p-2 focus:outline-none"
+                />
+              </span>
+            </div>
+            {errors.username && (
+              <p className="ml-4 mt-1 text-sm text-red-600">
+                {errors.username?.message}
+              </p>
+            )}
           </div>
           <p>
             By continuing, you agree to receive offers, news and updates from
@@ -74,8 +99,8 @@ export default function RegisterUsername() {
             disabled={!isDirty || !isValid}
             className={`w-full rounded-full py-3 font-semibold transition-all ${
               !isDirty || !isValid
-                ? "cursor-default bg-slate-200"
-                : "cursor-pointer bg-lime-300 hover:bg-lime-400 "
+                ? "cursor-default bg-slate-200 text-gray-500"
+                : "cursor-pointer bg-lime-300 text-black hover:bg-lime-400"
             }`}
           >
             Continue
